69730286ceeba0af1a9310e43c6724da2ba6f8f7
Rakib Hasan <rakib.hasan@bjitgroup.com>
2020-06-03 12:39:51 +0600

    Initial version of blog generated by JHipster-6.9.1

---
+++ src/main/java/extremecoder/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package extremecoder.repository;
+
+import extremecoder.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/extremecoder/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package extremecoder.service.dto;
